ecb5374441c2b90f3d1c748fb6e1bd0f93678bb4
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-06-20 12:55:24 +0530

    React portal is used for Overlays

--- src/components/UI/Overlays/Overlays.js
+++ src/components/UI/Overlays/Overlays.js
@@ -1,26 +1,33 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
-import Card from '../Card/Card';
-import CartList from '../../Cart/CartItems/CartList';
+import Card from "../Card/Card";
+import CartList from "../../Cart/CartItems/CartList";
 
-import classes from './Overlays.module.css';
+import classes from "./Overlays.module.css";
 
-const Backdrop = props => {
-  return <div className={classes.backdrop}>    
-  </div>
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
 };
 
-const Modal = props => {
-  return <Card className={classes.modal}>
-    <CartList />
-  </Card>
+const Modal = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <CartList />
+    </Card>
+  );
 };
 
-const Overlays = props => {
-  return <Fragment>
-    <Backdrop />
-    <Modal />
-  </Fragment>
+let portalEl = document.getElementById("overlays");
+
+const Overlays = (props) => {
+  return (
+    ReactDOM.createPortal(
+      <Fragment >
+        <Backdrop />
+        <Modal />
+      </Fragment>, portalEl)
+  );
 };
 
 export default Overlays;
